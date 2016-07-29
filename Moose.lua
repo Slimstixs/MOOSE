5130a7492bd78ea6e855d1032376b353357c22a1
Sven Van de Velde <sven.van.de.velde@telenet.be>
2016-07-29 09:55:53 +0200

    Updated static moose

---
+++ Moose.lua
@@ -0,0 +1,12 @@
+env.info( '*** MOOSE STATIC INCLUDE START *** ' ) 
+env.info( 'Moose Generation Timestamp: 20160729_0953' ) 
+local base = _G
+
+Include = {}
+Include.Files = {}
+Include.File = function( IncludeFile )
+end
+
+
+BASE:TraceOnOff( false )
+env.info( '*** MOOSE INCLUDE END *** ' ) 
